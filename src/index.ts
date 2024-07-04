c474bf875387a484946ef786fa600c9bb00dc970
Nurbek-web <taizhanovntm@gmail.com>
2024-07-04 17:33:02 +0500

    fix: mini bugs

--- src/index.ts
+++ src/index.ts
@@ -29,60 +29,64 @@ app.use(logger);
 app.use(express.json());
 app.use("/api/v1/", globalRouter);
 
-app.post("/upload-exterior", upload.single("image"), async (req: any, res) => {
-  try {
-    if (!req.file) {
-      return res.status(400).json({ error: "No file uploaded" });
-    }
-
-    const { materials, exteriorStyle, environment, time } = req.body;
-
-    if (!exteriorStyle || !environment || !time) {
-      return res.status(400).json({ error: "All form fields are required" });
-    }
-
-    const params: any = {
-      Bucket: process.env.S3_BUCKET_NAME,
-      Key: `uploads/${Date.now()}_${req.file.originalname}`,
-      Body: req.file.buffer,
-      ContentType: req.file.mimetype,
-    };
-
-    const s3Data = await s3.upload(params).promise();
-    const imageUrl = s3Data.Location;
-
-    // Generate a prompt based on the input fields
-    const prompt = `Generate a ${exteriorStyle} house exterior with ${materials.join(
-      ", "
-    )}, set in a ${environment} environment during the ${time}.`;
-
-    const output = await replicate.run(
-      "jagilley/controlnet-hough:854e8727697a057c525cdb45ab037f64ecca770a1769cc52287c2e56472a247b",
-      {
-        input: {
-          image: imageUrl,
-          eta: 0,
-          scale: 9,
-          a_prompt: "best quality, extremely detailed",
-          n_prompt:
-            "longbody, lowres, bad anatomy, bad hands, missing fingers, extra digit, fewer digits, cropped, worst quality, low quality",
-          ddim_steps: 20,
-          num_samples: "1",
-          value_threshold: 0.1,
-          image_resolution: "512",
-          detect_resolution: 512,
-          distance_threshold: 0.1,
-          prompt: prompt, // Use generated prompt
-        },
+app.post(
+  "/upload-exterior",
+  upload.single("image"),
+  async (req: any, res: any) => {
+    try {
+      if (!req.file) {
+        return res.status(400).json({ error: "No file uploaded" });
       }
-    );
+      const { materials, exteriorStyle, environment, time } = req.body;
+      console.log(req.body);
 
-    res.status(200).json(output);
-  } catch (error) {
-    console.error(error);
-    res.status(500).json({ error: "Something went wrong" });
+      if (!exteriorStyle || !environment || !time) {
+        return res.status(400).json({ error: "All form fields are required" });
+      }
+
+      const params: any = {
+        Bucket: process.env.S3_BUCKET_NAME,
+        Key: `uploads/${Date.now()}_${req.file.originalname}`,
+        Body: req.file.buffer,
+        ContentType: req.file.mimetype,
+      };
+
+      const s3Data = await s3.upload(params).promise();
+      const imageUrl = s3Data.Location;
+
+      // Generate a prompt based on the input fields
+      const prompt = `Generate a ${exteriorStyle} house exterior with ${JSON.parse(
+        materials
+      ).join(", ")}, set in a ${environment} environment during the ${time}.`;
+
+      const output = await replicate.run(
+        "jagilley/controlnet-hough:854e8727697a057c525cdb45ab037f64ecca770a1769cc52287c2e56472a247b",
+        {
+          input: {
+            image: imageUrl,
+            eta: 0,
+            scale: 9,
+            a_prompt: "best quality, extremely detailed",
+            n_prompt:
+              "longbody, lowres, bad anatomy, bad hands, missing fingers, extra digit, fewer digits, cropped, worst quality, low quality",
+            ddim_steps: 20,
+            num_samples: "1",
+            value_threshold: 0.1,
+            image_resolution: "512",
+            detect_resolution: 512,
+            distance_threshold: 0.1,
+            prompt: prompt, // Use generated prompt
+          },
+        }
+      );
+
+      res.status(200).json(output);
+    } catch (error) {
+      console.error(error);
+      res.status(500).json({ error: "Something went wrong" });
+    }
   }
-});
+);
 
 app.listen(PORT, () => {
   console.log(`Server runs at http://localhost:${PORT}`);
